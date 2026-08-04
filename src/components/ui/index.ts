f58fb307edafc7b95bee7e8592b804129575da2b
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-08-04 16:09:46 +0530

    latest

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,6 @@
 export * from "./badge";
 export * from "./button";
 export * from "./container";
-export * from "./newsletter-form";
 export * from "./product-card";
 export * from "./text-reveal";
 export * from "./custom-cursor";
